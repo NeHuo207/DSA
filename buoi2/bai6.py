1e0650b9bc833fcfc82a0fa2a965cc05acaf0e0c
NeHuo207 <kunhoknam666@gmail.com>
2026-06-03 16:38:23 +0700

    Create bai6.py

---
+++ buoi2/bai6.py
@@ -0,0 +1,9 @@
+def lower_bound(a, x):
+    lo, hi = 0, len(a)   
+    while lo < hi:
+        mid = (lo + hi) // 2
+        if a[mid] < x: lo = mid + 1
+        else: hi = mid
+    return lo  
+
+print(lower_bound([1, 3, 5, 7],4))
