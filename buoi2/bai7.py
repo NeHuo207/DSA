3eee27a862ef3396c269ba14992583e163c5fff8
NeHuo207 <kunhoknam666@gmail.com>
2026-06-03 16:38:18 +0700

    Create bai7.py

---
+++ buoi2/bai7.py
@@ -0,0 +1,9 @@
+def upper_bound(a, x):
+    lo, hi = 0, len(a)
+    while lo < hi:
+        mid = (lo + hi) // 2
+        if a[mid] <= x: lo = mid + 1
+        else: hi = mid
+    return lo
+
+print(upper_bound([1, 3, 5, 7],5))
