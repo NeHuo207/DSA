e87c48e1f0e5acca2384fe0449b3bac93b899294
NeHuo207 <kunhoknam666@gmail.com>
2026-06-03 16:38:16 +0700

    Create bai9.py

---
+++ buoi2/bai9.py
@@ -0,0 +1,9 @@
+def search_insert(a, x):
+    lo, hi = 0, len(a)
+    while lo < hi:
+        mid = (lo + hi) // 2
+        if a[mid] < x: lo = mid + 1
+        else: hi = mid
+    return lo
+
+print(search_insert([1, 3, 5, 6],4))
