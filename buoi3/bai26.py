cc26f70b0b39b61679cd9d60ef20cf57f0aa1dd9
NeHuo207 <kunhoknam666@gmail.com>
2026-06-17 16:16:29 +0700

    Create bai26.py

---
+++ buoi3/bai26.py
@@ -0,0 +1,9 @@
+def move_min_to_front(a):
+    min_idx = 0
+    for i in range(1, len(a)):
+        if a[i] < a[min_idx]:
+            min_idx = i
+    a[0], a[min_idx] = a[min_idx], a[0]
+    return a
+ 
+print("Bài 1:", move_min_to_front([4, 2, 7, 1, 3]))  # [1, 2, 7, 4, 3]
